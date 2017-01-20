f9bc31e21f4cb2097d06fb25fefa0c6603c89f6b
PHedro <pedrohenrique.reis@gmail.com>
2017-01-19 23:22:57 -0200

    fix middleware

--- core/middleware.py
+++ core/middleware.py
@@ -1,12 +1,13 @@
 import json
 
+from copy import copy
 from django.utils import timezone
 
 from core.models import BreadCrumbs
 
 
-class HanselAndGretelMiddleware(object):
-    def process_request(self, request):
+def hansel_and_gretel_middleware(get_response):
+    def breadcrumbs_path(request):
         params = {
             'user': request.user,
             'user_agent': request.META.get('HTTP_USER_AGENT'),
@@ -19,12 +20,15 @@ class HanselAndGretelMiddleware(object):
             'deleted_at': None,
             'deleted': False
         }
-        _get = request.GET
+        _get = copy(request.GET)
         if 'password' in _get.keys():
             _get.update({'password': '*******'})
         params.update({'get': json.dumps(_get)})
-        _post = request.POST
+        _post = copy(request.POST)
         if 'password' in _post.keys():
             _post.update({'password': '*******'})
         params.update({'post': json.dumps(_post)})
         BreadCrumbs.objects.create(**params)
+        response = get_response(request)
+        return response
+    return breadcrumbs_path
